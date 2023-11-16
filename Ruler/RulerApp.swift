de5668149428870b6447dcbc965297bc3e016176
Michalis Menesidis <mmenesidis@Michaliss-Air.home>
2023-11-16 20:18:21 +0200

    Initial commit

---
+++ Ruler/RulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RulerApp.swift
+//  Ruler
+//
+//  Created by Michalis Menesidis on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
