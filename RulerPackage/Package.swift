031bac15d63d965831c543be8d246498b4a7bf1f
Michalis Menesidis <menesidis@gmail.com>
2023-12-01 00:39:47 +0200

    Add composable architecture in RulerPackage

--- RulerPackage/Package.swift
+++ RulerPackage/Package.swift
@@ -12,11 +12,18 @@ let package = Package(
             name: "RulerPackage",
             targets: ["RulerPackage"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.4.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RulerPackage"),
+            name: "RulerPackage",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
         .testTarget(
             name: "RulerPackageTests",
             dependencies: ["RulerPackage"]),
