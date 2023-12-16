e19af98d756eab2ca4dbb7f14fabf5cadc3847e6
Michalis Menesidis <menesidis@gmail.com>
2023-12-16 22:15:30 +0200

    Update reducer since we have multiple selections

--- RulerPackage/Sources/RulerPackage/Reducer/MemoryReducer.swift
+++ RulerPackage/Sources/RulerPackage/Reducer/MemoryReducer.swift
@@ -1,16 +1,41 @@
 import ComposableArchitecture
 
 @Reducer
-struct MemoryFeature {
-    struct State: Equatable {
-        let items = [Item]()
+public struct MemoryFeature {
+    public init() {}
+    
+    public struct State: Equatable {
+        public let items: [[Item]]
+        public var selectedItems: [Item.ID : Item] = [:]
+        
+        public init(items: [[Item]]) {
+            self.items = items
+        }
+        
+        public func isSelected(item: Item) -> Bool {
+            let item = selectedItems[item.id]
+            return item.isNotNil
+        }
     }
     
-    enum Action: Equatable {}
+    public enum Action: Equatable {
+        case didTapItem(Item)
+    }
     
-    var body: some Reducer<State, Action> {
+    public var body: some Reducer<State, Action> {
         Reduce { state, action in
-            return .none
+            switch action {
+            case let .didTapItem(item):
+                
+                if let item = state.selectedItems[item.id] {
+                    // Item already exist
+                    state.selectedItems[item.id] = nil
+                } else {
+                    /// Save selected item
+                    state.selectedItems[item.id] = item
+                }
+                return .none
+            }
         }
     }
 }
